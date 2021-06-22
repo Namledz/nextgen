85b88facf74c965f1e23b28b2e4980d98bf40b5e
Coup Oab <dgbaoquoc@gmail.com>
2021-06-22 18:30:42 +0700

    analysis detail

--- src/app/modules/analysis/analysis-detail/analysis-detail.component.ts
+++ src/app/modules/analysis/analysis-detail/analysis-detail.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, Input, OnInit, ChangeDetectorRef, Inject, ElementRef, ViewChild } from '@angular/core';
 import * as IGV from 'igv';
 import { MatTab } from '@angular/material/tabs';
@@ -12,6 +13,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AnalysisDetailComponent implements OnInit {
 	id: any;
+	asideState: boolean = true
 	analysisName: string;
 	isLoaded: boolean;
 	type: string;
@@ -24,10 +26,72 @@ export class AnalysisDetailComponent implements OnInit {
 
 	igv: any;
 
-	chromosome = [
-		'Chrom 1',
-		'Chrom 2',
-		'Chrom 3'
+	chromosomeList = [
+		'Chr 1',
+		'Chr 2',
+		'Chr 3',
+		'Chr 4',
+		'Chr 5',
+		'Chr 6',
+		'Chr 7',
+		'Chr 8',
+		'Chr 9',
+		'Chr 10',
+		'Chr 11',
+		'Chr 12',
+		'Chr 13',
+		'Chr 14',
+		'Chr 15',
+		'Chr 16',
+		'Chr 17',
+		'Chr 18',
+		'Chr 19',
+		'Chr 20',
+		'Chr 21',
+		'Chr 22',
+		'Chr X',
+		'Chr Y',
+		'Chr MT'
+	]
+
+	annotationList = [
+		"5' UTR",
+		"Inframe deletion",
+		"Splice donor",
+		"Frameshift",
+		"Coding",
+		"other",
+		"Upstream",
+		"Synonymous",
+		"Noncoding transcript",
+		"3' UTR",
+		"NMD",
+		"Stop gained",
+		"Downstream",
+		"Noncoding exon",
+		"Inframe insertion",
+		"Regulatory region",
+		"Start retained",
+		"Protein altering variant",
+		"Intergenic",
+		"Stop retained",
+		"Start loss",
+		"Noncoding Transcript",
+		"Missense",
+		"Splice acceptor",
+		"Intron",
+		"Splice region",
+		"Stop lost",
+		"Start lost"
+	]
+
+	classificationList = [
+		"uncertain significance",
+		"likely benign",
+		"drug response",
+		"pathogenic",
+		"benign",
+		"likely pathogenic",
 	]
 
 	zygosity = [
@@ -53,7 +117,7 @@ export class AnalysisDetailComponent implements OnInit {
 		this.id = this.route.snapshot.params.id;
 		this.igv = IGV;
 		this.filterForm();
-		this.searchForm();
+		// this.searchForm();
 		this.isLoaded = false;
 		this.getAnalysisName();
 
@@ -84,20 +148,25 @@ export class AnalysisDetailComponent implements OnInit {
 	// filtration
 	filterForm() {
 		this.filterGroup = this.fb.group({
-			chromosome: [''],
-			gnomAdCompare: [''],
-			gnomAdText: [''],
-			qualityGreater: [''],
-			qualityLower: [''],
-			sequencenceOntology: [''],
-			depthGreater: [''],
-			depthLower: ['']
+			geneName: [''],
+			chromosome: [],
+			gnomADfrom: [''],
+			gnomADto: [''],
+			depthGreater: 0,
+			annotation: [''],
+			classification: [''],
+			alleleFraction: ['']
+			// qualityGreater: [''],
+			// qualityLower: [''],
+			// sequencenceOntology: [''],
+			// depthLower: [''],
+			// readDepth: ['']
 		});
 	}
 
 	applyFilter() {
 		let filter = this.filter();
-		console.log(this.filter());
+		console.log("Filter", this.filter());
 		this.variantListService.patchState({ filter });
 	}
 
@@ -105,7 +174,13 @@ export class AnalysisDetailComponent implements OnInit {
 		let filter = {};
 		const chromosome = this.filterGroup.get('chromosome').value;
 		if (chromosome) {
-			filter['chrom'] = chromosome;
+			let arrayInt = []
+			chromosome.forEach(e => {
+				let tmp = e.split(" ")
+				arrayInt.push(parseInt(tmp[1]))
+			})
+			console.log(arrayInt)
+			filter['chrom'] = arrayInt;
 		}
 
 		const depthGreater = this.filterGroup.get('depthGreater').value;
@@ -113,48 +188,74 @@ export class AnalysisDetailComponent implements OnInit {
 			filter['depth_greater'] = depthGreater;
 		}
 
-		const depthLower = this.filterGroup.get('depthLower').value;
-		if (depthLower) {
-			filter['depth_lower'] = depthLower;
+		const gnomADfrom = this.filterGroup.get('gnomADfrom').value;
+		if (gnomADfrom) {
+			filter['gnomADfrom'] = gnomADfrom;
 		}
 
-		const qualityGreater = this.filterGroup.get('qualityGreater').value;
-		if (qualityGreater) {
-			filter['quality_greater'] = qualityGreater;
+		const gnomADto = this.filterGroup.get('gnomADto').value;
+		if (gnomADto) {
+			filter['gnomADto'] = gnomADto;
 		}
 
-		const qualityLower = this.filterGroup.get('qualityLower').value;
-		if (qualityLower) {
-			filter['quality_lower'] = qualityLower;
-		}
+		// const sequencenceOntology = this.filterGroup.get('sequencenceOntology').value;
+		// if (sequencenceOntology) {
+		// 	filter['sequencence_ontology'] = sequencenceOntology;
+		// }
 
-		const gnomAdCompare = this.filterGroup.get('gnomAdCompare').value;
-		const filterGnomAdText = this.filterGroup.get('gnomAdText').value;
-		if (gnomAdCompare && filterGnomAdText) {
-			filter['gnomad'] = {
-				type: gnomAdCompare,
-				value: filterGnomAdText
-			}
-		}
-
-		const sequencenceOntology = this.filterGroup.get('sequencenceOntology').value;
-		if (sequencenceOntology) {
-			filter['sequencence_ontology'] = sequencenceOntology;
-		}
-
-		const geneName = this.searchGroup.get('geneName').value
+		const geneName = this.filterGroup.get('geneName').value
 		if (geneName) {
 			filter['gene'] = geneName;
 		}
 
+		const annotation = this.filterGroup.get('annotation').value
+		if (annotation) {
+			filter['annotation'] = annotation;
+		}
+
+		const classification = this.filterGroup.get('classification').value
+		if (classification) {
+			filter['classification'] = classification;
+		}
+
+		const alleleFraction = this.filterGroup.get('alleleFraction').value
+		if (alleleFraction) {
+			filter['alleleFraction'] = alleleFraction;
+		}
+		// const depthLower = this.filterGroup.get('depthLower').value;
+		// if (depthLower) {
+		// 	filter['depth_lower'] = depthLower;
+		// }
+
+		// const qualityGreater = this.filterGroup.get('qualityGreater').value;
+		// if (qualityGreater) {
+		// 	filter['quality_greater'] = qualityGreater;
+		// }
+
+		// const qualityLower = this.filterGroup.get('qualityLower').value;
+		// if (qualityLower) {
+		// 	filter['quality_lower'] = qualityLower;
+		// }
+
+		// const gnomAdCompare = this.filterGroup.get('gnomAdCompare').value;
+		// const filterGnomAdText = this.filterGroup.get('gnomAdText').value;
+		// if (gnomAdCompare && filterGnomAdText) {
+		// 	filter['gnomad'] = {
+		// 		type: gnomAdCompare,
+		// 		value: filterGnomAdText
+		// 	}
+		// }
+
+
+
 		return filter;
 	}
 
-	searchForm() {
-		this.searchGroup = this.fb.group({
-			geneName: [''],
-		});
-	}
+	// searchForm() {
+	// 	this.searchGroup = this.fb.group({
+	// 		geneName: [''],
+	// 	});
+	// }
 
 	search(searchTerm: string) {
 		this.variantListService.patchState({ searchTerm });
@@ -164,5 +265,25 @@ export class AnalysisDetailComponent implements OnInit {
 		return value;
 	}
 
+	toggleAside() {
+		if (this.asideState) {
+			this.asideState = false
+			document.getElementById("aside").style.width = '0'
+			document.getElementById("aside").style.padding = '0'
+			document.getElementById("aside").style.border = 'none'
+			document.getElementById("content-container").style.width = '98%'
+			document.getElementById("filter-conatiner").style.display = 'none'
+
+		} else {
+			this.asideState = true
+			document.getElementById("aside").style.width = '15%'
+			document.getElementById("aside").style.padding = '20px'
+			document.getElementById("aside").style.border = '1px solid #dee2e6'
+			document.getElementById("content-container").style.width = '85%'
+			document.getElementById("filter-conatiner").style.display = 'block'
+		}
+
+
+	}
 
 }
