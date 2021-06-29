db9c719ba0c3be3777d7772f76d0ce7bb1381d0a
atuan2408 <a.t240899@gmail.com>
2021-06-29 18:52:06 +0700

    update

--- src/app/modules/workspaces/workspaces-dashboard/workspaces-dashboard.component.ts
+++ src/app/modules/workspaces/workspaces-dashboard/workspaces-dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { WorkspacesService } from '../services/workspaces.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'app-workspaces-dashboard',
@@ -9,12 +10,14 @@ import { WorkspacesService } from '../services/workspaces.service';
 export class WorkspacesDashboardComponent implements OnInit {
 	info: any
 	@Input() id: any;
-
+	data: any;
+	show: boolean
+	
 	constructor(
 		private workspaceService: WorkspacesService,
-
-	) { }
-
+		private cd: ChangeDetectorRef,
+		private toastr: ToastrService
+		) { }		
 	ngOnInit(): void {
 		this.getWorkspaceDashboard()
 	}
@@ -22,8 +25,26 @@ export class WorkspacesDashboardComponent implements OnInit {
 
 	getWorkspaceDashboard() {
 		this.workspaceService.getWorkspaceDashboard(this.id).subscribe((response: any) => {
-			console.log(response)
+			if (response.status == "success") {
+				this.data = response.data
+			}
+			this.cd.detectChanges();
 		})
 	}
 
+	save() {
+		let uploadData = this.data.replaceAll("\n", "<br>")
+		console.log(uploadData)
+		let info = {
+			dashboardInfo: this.data,
+			id: this.id
+		}
+		this.workspaceService.updateWorkspaceDashboard(info).subscribe((response: any)=> {
+			if (response.status == "success") {
+				this.toastr.success(response.message)
+				this.show = false
+			}
+			this.cd.detectChanges();
+		})
+	}
 }
